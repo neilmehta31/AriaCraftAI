a91dcbd3e1e5b7ba5937f9547f120b2d3a8a9854
Neil Mehta <neil.mehta310501@gmail.com>
2024-03-12 13:49:54 +0530

    remove unused IERC721

--- contracts/contracts/MusicalNft.sol
+++ contracts/contracts/MusicalNft.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 import "hardhat/console.sol";
-import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MusicalNFT is ERC721 {
